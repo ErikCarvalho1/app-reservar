55efcffc865ef9bb9280bcdf9b4c6fb82622d19d
ERIK CARVALHO DA SILVA <erik.csilva7@senacsp.edu.br>
2025-11-25 11:46:44 -0300

    ++

---
+++ src/app/reserva/reserva.page.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmailValidator, FormControl, FormControlDirective, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar , ToastController} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-reserva',
+  templateUrl: './reserva.page.html',
+  styleUrls: ['./reserva.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class ReservaPage implements OnInit {
+  
+  reservaForm = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    telefone: new FormControl('',[Validators.required, Validators.minLength(10), Validators.maxLength(11)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    data: new FormControl('', [Validators.required]),
+    hora: new FormControl('',[Validators.required]),
+    pessoas: new FormControl(2,[Validators.min(1)]), 
+    preferencia: new FormControl(''),
+    ocasiao: new FormControl(''),
+    observacao: new FormControl(''), 
+  });
+
+  constructor(private toastCrtl: ToastController) {}
+  
+  async confirmaReserva(){
+    if(this.reservaForm.invalid){
+      this.mostrarToast("Preencha todos os campos obrigatórios");
+      return;
+    }
+    const novaReserva ={
+...this.reservaForm.value,
+criadoEm: new Date().toISOString()
+    };
+  }
+  async mostrarToast(msg: string) {
+    const toast = await this.toastCrtl.create({
+      message: msg,
+      duration: 2000,
+      color: 'primary', 
+      position: 'middle'
+      
+    });
+    toast.present();
+  }
+
+  ngOnInit() {
+  }
+
+}
